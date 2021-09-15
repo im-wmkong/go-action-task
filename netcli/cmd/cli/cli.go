02b793a4fbdf8a8579cd6efd57b9b7f3f157f73c
WmKong <im.wmkong@gmail.com>
2021-09-15 12:06:54 +0800

    netcli

--- netcli/cmd/cli/cli.go
+++ netcli/cmd/cli/cli.go
@@ -8,47 +8,77 @@ import (
 	"os"
 )
 
+func ns(context *cli.Context) error {
+	nss, err := net.LookupNS(context.Args().First())
+	if err != nil {
+		return err
+	}
+	for _, ns := range nss {
+		fmt.Println(*ns)
+	}
+	return nil
+}
+
+func cname(context *cli.Context) error {
+	cname, err := net.LookupCNAME(context.Args().First())
+	if err != nil {
+		return err
+	}
+	fmt.Println(cname)
+	return nil
+}
+
+func ip(context *cli.Context) error {
+	ips, err := net.LookupIP(context.Args().First())
+	if err != nil {
+		return err
+	}
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+	return nil
+}
+
+func help(context *cli.Context) error {
+	cmd := getCommands()
+	for _, v := range cmd {
+		fmt.Println(v.Name)
+	}
+	return nil
+}
+
+func getCommands() cli.Commands {
+	return cli.Commands{
+		{
+			Name:   "ns",
+			Usage:  "Looks Up the NameServers for a Particular Host",
+			Action: ns,
+		},
+		{
+			Name:   "cname",
+			Usage:  "Looks up the CNAME for a particular host",
+			Action: cname,
+		},
+		{
+			Name:   "ip",
+			Usage:  "Looks up the IP addresses for a particular host",
+			Action: ip,
+		},
+		{
+			Name:    "help",
+			Aliases: []string{"h"},
+			Usage:   "Shows a list of commands or help for one command",
+			Action:  help,
+		},
+	}
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "Website Lookup CLI"
 	app.Usage = "Let's you query IPs, CNAMEs and Name Servers!"
 	app.Version = "0.0.0"
-
-	app.Commands = cli.Commands{
-		{
-			Name:  "ns",
-			Usage: "Looks Up the NameServers for a Particular Host",
-			Action: func(context *cli.Context) error {
-				ns, err := net.LookupNS(context.Args().Get(0))
-				if err != nil {
-					return err
-				}
-				fmt.Println(ns)
-				return nil
-			},
-		},
-		{
-			Name:  "cname",
-			Usage: "Looks up the CNAME for a particular host",
-			Action: func(context *cli.Context) error {
-				return nil
-			},
-		},
-		{
-			Name:  "ip",
-			Usage: "Looks up the IP addresses for a particular host",
-			Action: func(context *cli.Context) error {
-				return nil
-			},
-		},
-		{
-			Name:  "help",
-			Usage: "Shows a list of commands or help for one command",
-			Action: func(context *cli.Context) error {
-				return nil
-			},
-		},
-	}
+	app.Commands = getCommands()
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
